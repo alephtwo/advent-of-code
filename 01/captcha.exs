398203a6263fac5e6cbe2f8166b7826ad801310e
Ben Christianson <thhuntertgm@gmail.com>
2017-12-01 11:56:35 -0600

    Solved day one, part two

--- 01/captcha.exs
+++ 01/captcha.exs
@@ -2,13 +2,25 @@ numbers = "input.txt"
   |> File.read!
   |> String.trim
   |> String.split("", trim: true)
-
-range = (-1 .. (Enum.count(numbers) - 1))
-matches_next = fn n -> (Enum.at(numbers, n) == Enum.at(numbers, n + 1)) end
-range
-  |> Enum.filter(matches_next)
-  |> Enum.map(fn x -> Enum.at(numbers, x) end)
   |> Enum.map(&Integer.parse/1)
   |> Enum.map(fn {x, _} -> x end)
-  |> Enum.reduce(0, &Kernel.+/2)
-  |> IO.puts
+
+count = Enum.count(numbers)
+halfway_around = div(count, 2)
+range = (-count .. -1)
+
+print_captcha = fn filter ->
+  range
+    |> Enum.filter(filter)
+    |> Enum.map(fn x -> Enum.at(numbers, x) end)
+    |> Enum.reduce(0, &Kernel.+/2)
+    |> IO.puts
+end
+
+matches_next = fn n -> (Enum.at(numbers, n) == Enum.at(numbers, n + 1)) end
+print_captcha.(matches_next)
+
+matches_halfway_around = fn n ->
+  Enum.at(numbers, n) == Enum.at(numbers, n + halfway_around)
+end
+print_captcha.(matches_halfway_around)
