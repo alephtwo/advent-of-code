19b1cfa9fa79164702d68802966a43fff3f4ce36
Ben Christianson <thhuntertgm@gmail.com>
2017-12-05 15:15:24 -0600

    Added assertions for day 5 answers

--- test/day05_test.exs
+++ test/day05_test.exs
@@ -7,8 +7,16 @@ defmodule DayFiveTest do
     assert result == {5, [2, 5, 0, 1, -2]}
   end
 
+  # test "part one actual (cursed, do not run)" do
+  #   assert DayFive.part_one(DayFive.get_input()) == 354121
+  # end
+
   test "part two dummy" do
     result = DayFive.steps_to_escape_part_two([0, 3, 0, 1, -3])
     assert result == {10, [2, 3, 2, 3, -1]}
   end
+
+  # test "part two actual (cursed, do not run!)" do
+  #   assert DayFive.part_two(DayFive.get_input()) == 27283023
+  # end
 end
