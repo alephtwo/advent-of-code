7556246abf3b15cdb4c09eb0fcc342bc22c0c236
Ben Christianson <thhuntertgm@gmail.com>
2017-12-05 11:34:19 -0600

    Added tests for day 4, part 2

--- lib/day_04.ex
+++ lib/day_04.ex
@@ -4,18 +4,29 @@ defmodule DayFour do
 
   def solve do
     IO.puts part_one(parse_file())
+    IO.puts part_two(parse_file())
   end
 
   def part_one(input) do
     input |> Enum.filter(&contains_no_duplicate_words/1) |> Enum.count
   end
 
+  def part_two(input) do
+    input |> Enum.filter(&contains_no_anagrams/1) |> Enum.count
+  end
+
   def contains_no_duplicate_words(passphrase) do
-    tokens = String.split(passphrase, " ", trim: true)
+    tokens = split_passphrase(passphrase)
     Enum.count(tokens |> Enum.uniq) == Enum.count(tokens)
+  end
+
+  def contains_no_anagrams(passphrase) do
+    tokens = split_passphrase(passphrase)
   end
 
   def parse_file do
     @input |> String.split("\n", trim: true)
   end
+
+  defp split_passphrase(p), do: String.split(p, " ", trim: true)
 end
--- test/day04_test.exs
+++ test/day04_test.exs
@@ -17,4 +17,24 @@ defmodule DayFourTest do
   test "part one actual" do
     assert DayFour.part_one(DayFour.parse_file()) == 451
   end
+
+  test "part two dummy valid" do
+    assert DayFour.contains_no_anagrams("abcde fghij")
+  end
+
+  test "part two dummy invalid" do
+    assert !DayFour.contains_no_anagrams("abcde xyz ecdab")
+  end
+
+  test "part two dummy valid - all characters must be used in an anagram" do
+    assert DayFour.contains_no_anagrams("a ab abc abd abf abj")
+  end
+
+  test "part two dummy valid - ioioioio" do
+    assert DayFour.contains_no_anagrams("iiii oiii ooii oooi oooo")
+  end
+
+  test "part two dummy invalid - ioioioio" do
+    assert !DayFour.contains_no_anagrams("oiii ioii iioi iiio")
+  end
 end
