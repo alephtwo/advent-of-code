efe64201b6238bf8d93804cb353813aa960d814b
Ben Christianson <thhuntertgm@gmail.com>
2017-12-06 10:26:25 -0600

    Added failing tests

--- test/day06_test.exs
+++ test/day06_test.exs
@@ -6,6 +6,13 @@ defmodule DaySixTest do
     assert DaySix.choose_next_bank([0, 2, 7, 0]) == {7, 2}
   end
 
+  test "part one update banks" do
+    assert DaySix.update_banks([0, 2, 7, 0]) == [2, 4, 1, 2]
+    assert DaySix.update_banks([2, 4, 1, 2]) == [3, 1, 2, 3]
+    assert DaySix.update_banks([3, 1, 2, 3]) == [0, 2, 3, 4]
+    assert DaySix.update_banks([1, 3, 4, 1]) == [2, 4, 1, 2]
+  end
+
   test "part one rotate left" do
     assert DaySix.rotate_left([0, 1, 2], -1) == [2, 0, 1]
     assert DaySix.rotate_left([0, 1, 2], 0) == [0, 1, 2]
