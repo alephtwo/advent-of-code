d5b6ba0e1b09cf79c3d2c4a52912696c2a91c3cd
Ben Christianson <thhuntertgm@gmail.com>
2017-12-11 10:14:49 -0600

    Successfully parsing input

--- lib/day_07.ex
+++ lib/day_07.ex
@@ -1,4 +1,19 @@
 defmodule DaySeven do
   @external_resource "priv/07.txt"
   @input File.read! "priv/07.txt"
+
+  def parse_string(string) do
+    tokens = String.split(string, "-> ", trim: true)
+
+    disc = parse_name_and_weight(Enum.at(tokens, 0))
+    Map.put(disc, :children, parse_children(Enum.at(tokens, 1)))
+  end
+
+  defp parse_name_and_weight(string) do
+    [[_, name, weight]] = Regex.scan(~r/^(.*)? \((\d+)\)/, string)
+    %{ name: name, weight: String.to_integer(weight) }
+  end
+
+  defp parse_children(nil), do: []
+  defp parse_children(s), do: String.split(s, ", ", trim: true)
 end
--- test/day07_test.exs
+++ test/day07_test.exs
@@ -2,4 +2,22 @@ defmodule DaySevenTest do
   use ExUnit.Case
   doctest DaySeven
 
+  test "parse string, no children" do
+    parsed = DaySeven.parse_string("pbga (66)")
+    assert parsed == %{weight: 66, name: "pbga", children: []}
+  end
+
+  test "parse string, one child" do
+    parsed = DaySeven.parse_string("fwft (72) -> ktlj")
+    assert parsed == %{ weight: 72, name: "fwft", children: ["ktlj"] }
+  end
+
+  test "parse string, multiple children" do
+    parsed = DaySeven.parse_string("fwft (72) -> ktlj, cntj, xhth")
+    assert parsed == %{
+      weight: 72,
+      name: "fwft",
+      children: ["ktlj", "cntj", "xhth"]
+    }
+  end
 end
