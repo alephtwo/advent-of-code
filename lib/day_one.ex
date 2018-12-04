3cc53921b0a109fb7da49be508ce65157cf6a082
Ben Christianson <thhuntertgm@gmail.com>
2018-12-04 12:51:43 -0600

    Stub out day one part two

--- lib/day_one.ex
+++ lib/day_one.ex
@@ -7,13 +7,32 @@ defmodule DayOne do
 
   def solve do
     IO.puts(part_one(parse_input()))
+    IO.puts(part_two(parse_input()))
   end
 
   @doc """
   Day One, Part One.
-  Accepts input as a list and performs the appropriate transformations.
+  Accepts input as a list and returns the sum of all values.
   """
   def part_one(input) when is_list(input), do: Enum.sum(input)
+
+  @doc """
+  Repeat the cycle until we find a duplicate frequency.
+  TODO: Boy, this sure is inefficient. But all the tests pass, so ¯\_(ツ)_/¯
+  """
+  def part_two(input) when is_list(input) do
+    input
+    |> Stream.cycle()
+    |> Enum.reduce_while([0], fn n, accum ->
+      next_freq = Enum.at(accum, 0) + n
+
+      if Enum.member?(accum, next_freq) do
+        {:halt, next_freq}
+      else
+        {:cont, [next_freq | accum]}
+      end
+    end)
+  end
 
   def parse_input do
     @input
--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -1,4 +1,7 @@
 defmodule DayOneTest do
+  @moduledoc """
+  Tests for day one.
+  """
   use ExUnit.Case
   doctest DayOne
 
@@ -17,4 +20,25 @@ defmodule DayOneTest do
   test "part one: solution" do
     assert DayOne.part_one(DayOne.parse_input()) == 402
   end
+
+  test "part two: first example" do
+    assert DayOne.part_two([+1, -1]) == 0
+  end
+
+  test "part two: second example" do
+    assert DayOne.part_two([+3, +3, +4, -2, -4]) == 10
+  end
+
+  test "part two: third example" do
+    assert DayOne.part_two([-6, +3, +8, +5, -6]) == 5
+  end
+
+  test "part two: fourth example" do
+    assert DayOne.part_two([+7, +7, -2, -7, -4]) == 14
+  end
+
+  # This test is really quite slow...
+  # test "part two: solution" do
+  #   assert DayOne.part_two(DayOne.parse_input()) == 481
+  # end
 end
