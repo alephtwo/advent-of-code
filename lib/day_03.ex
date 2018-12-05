f3b5fda3fce22c0af3d5ccef83c44d65dcb9b9e2
Ben Christianson <thhuntertgm@gmail.com>
2018-12-04 22:44:25 -0600

    Parse input from Day 3

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -4,4 +4,24 @@ defmodule Day03 do
   """
   @external_resource "priv/03.txt"
   @input File.read!("priv/03.txt")
+
+  def part_one, do: part_one(parse_input())
+
+  def part_one(input) do
+    claims = Enum.map(input, &parse_claim/1)
+  end
+
+  def part_two, do: part_two(parse_input())
+
+  def part_two(_input) do
+  end
+
+  defp parse_input, do: String.split(@input, "\n", trim: true)
+
+  defp parse_claim(claim) do
+    Regex.named_captures(
+      ~r/#(?<id>\d+) @ (?<left>\d+),(?<top>\d+): (?<height>\d+)x(?<width>\d+)/,
+      claim
+    )
+  end
 end
--- test/day_03_test.exs
+++ test/day_03_test.exs
@@ -4,4 +4,14 @@ defmodule Day03Test do
   """
   use ExUnit.Case
   doctest Day03
+
+  test "part one: example" do
+    overlaps = Day03.part_one([
+      "#1 @ 1,3: 4x4",
+      "#2 @ 3,1: 4x4",
+      "#3 @ 5,5: 2x2"
+    ])
+
+    assert overlaps == 4
+  end
 end
