7ee733fae9ad9b8a3e082be31b9efdccded52ec9
Ben Christianson <thhuntertgm@gmail.com>
2018-12-05 13:46:38 -0600

    Parse input for day four

--- lib/day_04.ex
+++ lib/day_04.ex
@@ -3,19 +3,27 @@ defmodule Day04 do
   Day Four of Advent of Code 2018.
   """
   @external_resource "priv/04.txt"
-  @input File.read!("priv/04.txt")
+  @input File.read!("priv/04.txt") |> String.split("\n", trim: true)
 
-  def part_one, do: part_one(parse_input())
+  def part_one, do: part_one(@input)
 
-  def part_one(_input) do
+  def part_one(input) do
+    input
+    |> Enum.map(&parse_line/1)
+    |> IO.puts()
   end
 
-  def part_two, do: part_two(parse_input())
+  def part_two, do: part_two(@input)
 
   def part_two(_input) do
   end
 
-  defp parse_input do
-    String.split(@input, "\n", trim: true)
+  defp parse_line(line) do
+    parsed = Regex.named_captures(~r/^\[(?<time>.*?)\] (?<action>.*?)$/, line)
+
+    fake_iso_date = parsed["time"] <> ":00Z"
+    {:ok, timestamp} = DateTime.from_iso8601(fake_iso_date)
+
+    %{action: parsed["action"], timestamp: timestamp}
   end
 end
