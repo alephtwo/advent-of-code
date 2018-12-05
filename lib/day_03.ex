d03601fa7791c1e5163ccc523c793e1b21966364
Ben Christianson <thhuntertgm@gmail.com>
2018-12-04 19:15:10 -0600

    Add Day 3

---
+++ lib/day_03.ex
@@ -0,0 +1,7 @@
+defmodule Day03 do
+  @moduledoc """
+  Day Three of Advent of Code 2018.
+  """
+  @external_resource "priv/03.txt"
+  @input File.read!("priv/03.txt")
+end
---
+++ test/day_03_test.exs
@@ -0,0 +1,7 @@
+defmodule Day03Test do
+  @moduledoc """
+  Tests for day three.
+  """
+  use ExUnit.Case
+  doctest Day03
+end
