507e83004f60bd154186e4fa94dc004f5a24ec90
Ben Christianson <thhuntertgm@gmail.com>
2019-12-01 17:41:26 -0600

    Solve part one

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,5 +1,13 @@
 defmodule Day01 do
+  @input File.read!("priv/01.txt")
+         |> String.split("\n", trim: true)
+         |> Enum.map(&String.to_integer(&1))
+
   def part_one do
-    :ok
+    @input
+    |> Enum.map(&calculate_fuel_from_mass(&1))
+    |> Enum.sum()
   end
+
+  def calculate_fuel_from_mass(mass), do: div(mass, 3) - 2
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -2,7 +2,23 @@ defmodule Day01Test do
   use ExUnit.Case
   doctest Day01
 
-  test "set up" do
-    assert Day01.part_one() == :ok
+  test "mass 12" do
+    assert Day01.calculate_fuel_from_mass(12) == 2
+  end
+
+  test "mass 14" do
+    assert Day01.calculate_fuel_from_mass(14) == 2
+  end
+
+  test "mass 1969" do
+    assert Day01.calculate_fuel_from_mass(1969) == 654
+  end
+
+  test "mass 100756" do
+    assert Day01.calculate_fuel_from_mass(100_756) == 33583
+  end
+
+  test "part one" do
+    assert Day01.part_one() == 3_342_946
   end
 end
