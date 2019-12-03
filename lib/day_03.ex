d779b364a2e62068733b8b5f713fe9356a759ca8
Ben Christianson <thhuntertgm@gmail.com>
2019-12-03 11:22:23 -0600

    Pull out path render to separate function

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -6,27 +6,32 @@ defmodule Day03 do
          |> String.split("\n", trim: true)
          |> Enum.map(fn x -> String.split(x, ",", trim: true) end)
 
+  @central_point {0, 0}
+
   def part_one do
     min_manhattan_distance(@input)
   end
 
   @spec min_manhattan_distance(list) :: number
   def min_manhattan_distance(wires) when is_list(wires) do
-    central_point = {0, 0}
+    [first, second] = get_paths(wires)
+    first
+    |> MapSet.delete(@central_point)
+    |> MapSet.intersection(second)
+    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+    |> Enum.min()
+  end
+
+  defp get_paths(wires) do
     accumulator = %{
-      paths: MapSet.new() |> MapSet.put(central_point),
-      current: central_point
+      paths: MapSet.new() |> MapSet.put(@central_point),
+      current: @central_point
     }
 
     [%{paths: first}, %{paths: second}] = Enum.map(wires, fn wire ->
       Enum.reduce(wire, accumulator, &reduce_path/2)
     end)
-
-    first
-    |> MapSet.delete({0, 0})
-    |> MapSet.intersection(second)
-    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
-    |> Enum.min()
+    [first, second]
   end
 
   defp reduce_path(instruction, %{paths: paths, current: {x, y}}) do
