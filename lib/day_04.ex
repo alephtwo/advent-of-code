626396cbc0bdb09fceb0b89514c885fc7ff5b826
Ben Christianson <thhuntertgm@gmail.com>
2019-12-03 23:14:01 -0600

    Check for duplicates

--- lib/day_04.ex
+++ lib/day_04.ex
@@ -2,10 +2,21 @@ defmodule Day04 do
   @input 273025..767253
 
   def part_one do
-
+    @input
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.filter(&passes_criteria/1)
+    |> Enum.count()
   end
 
   def passes_criteria(password) do
-    false
+    repeated_digit = String.match?(password, ~r"(\d)\1+")
+    {not_decreasing, _} =
+      password
+      |> String.split("", trim: true)
+      |> Enum.reduce({true, 0}, fn x, {satisfied, last} ->
+        {satisfied && x >= last, x}
+      end)
+
+    repeated_digit && not_decreasing
   end
 end
