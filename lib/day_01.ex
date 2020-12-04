4c70173e3762de85dd6c610a65ef7cac72ed313c
Ben Christianson <thhuntertgm@gmail.com>
2020-12-04 00:35:11 -0600

    Day 1, Part 2

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -10,15 +10,40 @@ defmodule Day01 do
     find_product_of_pair(@input)
   end
 
-  def find_product_of_pair(entries) do 
-    [a, b] = find_pair_that_adds_to_2020(entries)
-    a * b
+  def part_two do
+    find_product_of_triplet(@input)
   end
 
-  def find_pair_that_adds_to_2020(entries) when is_list(entries) do
+  def find_product_of_pair(entries) do
+    entries
+    |> find_pair_that_adds_to_2020
+    |> Enum.reduce(&*/2)
+  end
+
+  def find_product_of_triplet(entries) do
+    entries
+    |> find_triplet_that_adds_to_2020
+    |> Enum.reduce(&*/2)
+  end
+
+  def find_pair_that_adds_to_2020(entries) do
     pairs = for x <- entries, y <- entries, x != y, do: [x, y]
-    pairs 
-    |> Enum.filter(fn [x, y] -> x + y == 2020 end)
-    |> Enum.at(0)
+    find_entries_that_sum_to_2020(pairs)
+  end
+
+  def find_triplet_that_adds_to_2020(entries) do
+    triplets = for x <- entries, y <- entries, z <- entries, x != y, y != z, x != z, do: [x, y, z]
+    find_entries_that_sum_to_2020(triplets)
+  end
+
+  def find_entries_that_sum_to_2020(lists) do
+    lists
+    |> Stream.filter(&list_sum_equals_2020/1)
+    |> Enum.take(1)
+    |> List.flatten()
+  end
+
+  defp list_sum_equals_2020(list) do
+    Enum.sum(list) == 2020
   end
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -5,7 +5,7 @@ defmodule Day01Test do
   use ExUnit.Case
   doctest Day01
 
-  @sample [1721, 979, 366, 299, 675, 1456] 
+  @sample [1721, 979, 366, 299, 675, 1456]
 
   test "part 1 example find_pair_that_adds_to_2020" do
     assert Day01.find_pair_that_adds_to_2020(@sample) == [1721, 299]
@@ -16,6 +16,18 @@ defmodule Day01Test do
   end
 
   test "part 1 solution" do
-    assert Day01.part_one == 259716
+    assert Day01.part_one() == 259_716
+  end
+
+  test "part 2 example find_triplet_that_adds_to_2020" do
+    assert Day01.find_triplet_that_adds_to_2020(@sample) == [979, 366, 675]
+  end
+
+  test "part 2 example find_product_of_triplet" do
+    assert Day01.find_product_of_triplet(@sample) == 241_861_950
+  end
+
+  test "part 2 solution" do
+    assert Day01.part_two() == 120_637_440
   end
 end
