7eb19a8686498fc47b92b95860dd1038283aa2be
Ben Christianson <thhuntertgm@gmail.com>
2020-12-05 10:25:57 -0600

    Even more golfing

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -17,30 +17,25 @@ defmodule Day05 do
     |> Enum.at(0)
   end
 
-  def parse_boarding_pass(text) do
+  defp get_seat_ids() do
+    @input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_seat_id/1)
+  end
+
+  def parse_seat_id(text) do
     # The first 7 characters are to find your row. 
     # The remaining 3 are to find your column.
     {r, c} = String.split_at(text, 7)
-
-    row = resolve_binary_partition(r)
-    col = resolve_binary_partition(c)
-
-    %{row: row, column: col, seat_id: row * 8 + col}
+    partition(r) * 8 + partition(c)
   end
 
-  def resolve_binary_partition(instructions) do
+  def partition(instructions) do
     # The instructions can be converted directly to their
     # value by interpreting them as base 2 (with some fiddling).
     instructions
     |> String.replace(~r/(F|L)/, "0")
     |> String.replace(~r/(B|R)/, "1")
     |> String.to_integer(2)
-  end
-
-  defp get_seat_ids(input \\ @input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_boarding_pass/1)
-    |> Enum.map(fn x -> x.seat_id end)
   end
 end
--- test/day_05_test.exs
+++ test/day_05_test.exs
@@ -6,41 +6,22 @@ defmodule Day05Test do
   doctest Day05
 
   test "part 1 sample solutions" do
-    assert Day05.parse_boarding_pass("BFFFBBFRRR") == %{
-             row: 70,
-             column: 7,
-             seat_id: 567
-           }
-
-    assert Day05.parse_boarding_pass("FFFBBBFRRR") == %{
-             row: 14,
-             column: 7,
-             seat_id: 119
-           }
-
-    assert Day05.parse_boarding_pass("BBFFBBFRLL") == %{
-             row: 102,
-             column: 4,
-             seat_id: 820
-           }
+    assert Day05.parse_seat_id("BFFFBBFRRR") == 567
+    assert Day05.parse_seat_id("FFFBBBFRRR") == 119
+    assert Day05.parse_seat_id("BBFFBBFRLL") == 820
   end
 
   test "test binary partition" do
-    assert Day05.resolve_binary_partition("BFFFBBF") == 70
-    assert Day05.resolve_binary_partition("RRR") == 7
+    assert Day05.partition("BFFFBBF") == 70
+    assert Day05.partition("RRR") == 7
 
-    assert Day05.resolve_binary_partition("FFFBBBF") == 14
-    assert Day05.resolve_binary_partition("RRR") == 7
+    assert Day05.partition("FFFBBBF") == 14
+    assert Day05.partition("RRR") == 7
 
-    assert Day05.resolve_binary_partition("BBFFBBF") == 102
-    assert Day05.resolve_binary_partition("RLL") == 4
+    assert Day05.partition("BBFFBBF") == 102
+    assert Day05.partition("RLL") == 4
   end
 
-  test "part 1 solution" do
-    assert Day05.part_one() == 818
-  end
-
-  test "part 2 solution" do
-    assert Day05.part_two() == 559
-  end
+  test "part 1 solution", do: assert(Day05.part_one() == 818)
+  test "part 2 solution", do: assert(Day05.part_two() == 559)
 end
