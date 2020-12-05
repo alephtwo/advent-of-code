0a42b4ae2968d03a8d20febc9013ef696a5fc85a
Ben Christianson <thhuntertgm@gmail.com>
2020-12-04 23:19:53 -0600

    Stub out incorrect day 5 part 1

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -3,9 +3,43 @@ defmodule Day05 do
   Day 5 of Advent of Code 2020.
   """
 
+  @input File.read!("priv/05.txt")
+
   def part_one do
+    parse_input()
+    |> Enum.map(&parse_boarding_pass/1)
+    |> Enum.map(fn x -> x.seat_id end)
+    |> Enum.max()
   end
 
   def part_two do
+  end
+
+  def parse_boarding_pass(text) do
+    # The first 7 characters are to find your row. 
+    # The remaining 3 are to find your column.
+    {r, c} = String.split_at(text, 7)
+
+    row = follow_row_instructions(r)
+    col = follow_col_instructions(c)
+
+    %{
+      row: row,
+      column: col,
+      seat_id: row * 8 + col
+    }
+  end
+
+  def follow_row_instructions(instructions) do
+    1
+  end
+
+  def follow_col_instructions(instructions) do
+    1
+  end
+
+  defp parse_input(input \\ @input) do
+    @input
+    |> String.split("\n", trim: true)
   end
 end
