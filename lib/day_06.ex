e4d3ad468ef4d76dbf37c2188e8c6ebc59f471fb
Ben Christianson <thhuntertgm@gmail.com>
2020-12-06 14:06:56 -0600

    Start Day 6 Part 1

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -3,7 +3,19 @@ defmodule Day06 do
   Day 6 of Advent of Code 2020.
   """
 
-  def part_one, do: :error
+  @input File.read!("priv/06.txt")
+
+  def part_one(input \\ @input) do
+    input
+    |> parse_input()
+    |> IO.inspect()
+  end
 
   def part_two, do: :error
+
+  def parse_input(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.split(&1, "\n", trim: true))
+  end
 end
--- test/day_06_test.exs
+++ test/day_06_test.exs
@@ -5,11 +5,33 @@ defmodule Day06Test do
   use ExUnit.Case
   doctest Day06
 
+  @sample """
+  abc
+
+  a
+  b
+  c
+
+  ab
+  ac
+
+  a
+  a
+  a
+  a
+
+  b
+  """
+
   test "part 1 solution" do
     assert Day06.part_one() == :ok
   end
 
-  test "part 2 solution" do
-    assert Day06.part_two() == :ok
+  test "part 1 sample solution" do
+    assert Day07.part_one(@sample) == 11
   end
+
+  # test "part 2 solution" do
+  #   assert Day06.part_two() == :ok
+  # end
 end
