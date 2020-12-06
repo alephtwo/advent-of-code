23115b421cb83bcf83b5b661bf32523df49c63e3
Ben Christianson <thhuntertgm@gmail.com>
2020-12-06 14:25:37 -0600

    Refactor for smaller code

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -5,34 +5,23 @@ defmodule Day06 do
 
   @input File.read!("priv/06.txt")
 
-  def part_one(input \\ @input) do
+  def part_one(input \\ @input), do: sum_responses_using_function(input, &MapSet.union/2)
+
+  def part_two(input \\ @input), do: sum_responses_using_function(input, &MapSet.intersection/2)
+
+  defp sum_responses_using_function(input, function) do
     input
     |> parse_input()
-    |> Enum.map(&determine_group_yes_responses/1)
+    |> Enum.map(&determine_responses_using_function(&1, function))
     |> Enum.map(&Enum.count/1)
     |> Enum.sum()
   end
 
-  def part_two(input \\ @input) do
-    input
-    |> parse_input()
-    |> Enum.map(&determine_everyone_yes_responses/1)
-    |> Enum.map(&Enum.count/1)
-    |> Enum.sum()
-  end
-
-  defp determine_group_yes_responses(responses) do
+  defp determine_responses_using_function(responses, function) do
     responses
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(&MapSet.new/1)
-    |> Enum.reduce(&MapSet.union/2)
-  end
-
-  defp determine_everyone_yes_responses(responses) do
-    responses
-    |> Enum.map(&String.graphemes/1)
-    |> Enum.map(&MapSet.new/1)
-    |> Enum.reduce(&MapSet.intersection/2)
+    |> Enum.reduce(function)
   end
 
   defp parse_input(input) do
