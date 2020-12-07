7417b0ccea590bb22ed5143ccac5fa7dd74b5f75
Ben Christianson <thhuntertgm@gmail.com>
2020-12-06 23:25:34 -0600

    Parse the file

--- lib/day_07.ex
+++ lib/day_07.ex
@@ -3,9 +3,46 @@ defmodule Day07 do
   Day 7 of Advent of Code 2020.
   """
 
-  @input :ok
+  @input File.read!("priv/07.txt")
 
-  def part_one(input \\ @input), do: :error
+  def part_one(input \\ @input) do
+    input
+    |> parse_input()
+    |> IO.inspect()
+  end
 
   def part_two(input \\ @input), do: :error
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_rule/1)
+  end
+
+  def parse_rule(text) do
+    [type, contents] = String.split(text, " contain ", trim: true)
+
+    %{
+      # Remove " bags" because it will appear in everything.
+      type: String.replace(type, ~r/ bags?/, ""),
+      rules: parse_contents(contents)
+    }
+  end
+
+  def parse_contents(contents) do
+    contents
+    |> String.split(", ", trim: true)
+    |> Enum.map(&parse_bag/1)
+    |> Enum.filter(fn x -> x != :none end)
+  end
+
+  def parse_bag(bag) do
+    if bag == "no other bags." do
+      :none
+    else
+      regex = ~r/(?<amount>\d+) (?<color>.*?) bags?\.?/
+      caps = Regex.named_captures(regex, bag)
+      %{color: Map.get(caps, "color"), amount: caps |> Map.get("amount") |> String.to_integer()}
+    end
+  end
 end
--- test/day_07_test.exs
+++ test/day_07_test.exs
@@ -5,9 +5,25 @@ defmodule Day07Test do
   use ExUnit.Case
   doctest Day07
 
-  test "part 1 solution" do
-    assert Day07.part_one() == :ok
+  @sample """
+  light red bags contain 1 bright white bag, 2 muted yellow bags.
+  dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+  bright white bags contain 1 shiny gold bag.
+  muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+  shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+  dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+  vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+  faded blue bags contain no other bags.
+  dotted black bags contain no other bags.
+  """
+
+  test "part 1 sample solution" do
+    assert Day07.part_one(@sample) == 4
   end
+
+  # test "part 1 solution" do
+  #   assert Day07.part_one() == :ok
+  # end
 
   # test "part 2 solution" do
   #   assert Day07.part_two() == :ok
