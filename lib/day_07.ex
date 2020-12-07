ea68c530b151dd938841bfca836417fdc46ba4f0
Ben Christianson <thhuntertgm@gmail.com>
2020-12-06 23:43:29 -0600

    Build a graph... sure!

--- lib/day_07.ex
+++ lib/day_07.ex
@@ -7,19 +7,20 @@ defmodule Day07 do
 
   def part_one(input \\ @input) do
     input
-    |> parse_input()
+    |> parse_graph()
     |> IO.inspect()
   end
 
-  def part_two(input \\ @input), do: :error
+  def part_two(input \\ @input), do: input
 
-  def parse_input(input) do
+  defp parse_graph(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(&parse_rule/1)
+    |> build_graph()
   end
 
-  def parse_rule(text) do
+  defp parse_rule(text) do
     [type, contents] = String.split(text, " contain ", trim: true)
 
     %{
@@ -29,14 +30,34 @@ defmodule Day07 do
     }
   end
 
-  def parse_contents(contents) do
+  defp build_graph(rules) do
+    graph = :digraph.new()
+
+    # Add all nodes to the graph.
+    # Assume every type of bag has a line in the file...
+    rules
+    |> Enum.map(fn r -> r.type end)
+    |> Enum.each(fn t -> :digraph.add_vertex(graph, t) end)
+
+    # Ok, now add all edges.
+    rules
+    |> Enum.map(fn node ->
+      Enum.each(node.rules, fn rule ->
+        rule.color
+        |> List.duplicate(rule.amount)
+        |> Enum.each(fn color -> :digraph.add_edge(graph, node.type, color) end)
+      end)
+    end)
+  end
+
+  defp parse_contents(contents) do
     contents
     |> String.split(", ", trim: true)
     |> Enum.map(&parse_bag/1)
     |> Enum.filter(fn x -> x != :none end)
   end
 
-  def parse_bag(bag) do
+  defp parse_bag(bag) do
     if bag == "no other bags." do
       :none
     else
