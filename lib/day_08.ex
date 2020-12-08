52a3226297516adc81504ce8dff060acf34890bf
Ben Christianson <thhuntertgm@gmail.com>
2020-12-07 23:06:24 -0600

    Start Day 8

--- lib/day_08.ex
+++ lib/day_08.ex
@@ -2,9 +2,14 @@ defmodule Day08 do
   @moduledoc """
   Day 8 of Advent of Code 2020.
   """
-  @input :error
+  @input File.read!("08.txt")
 
   def part_one(input \\ @input), do: input
 
   def part_two(input \\ @input), do: input
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+  end
 end
--- test/day_08_test.exs
+++ test/day_08_test.exs
@@ -5,11 +5,27 @@ defmodule Day08Test do
   use ExUnit.Case
   doctest Day08
 
-  test "part 1 solution" do
-    assert Day08.part_one() == :ok
+  @sample """
+  nop +0
+  acc +1
+  jmp +4
+  acc +3
+  jmp -3
+  acc -99
+  acc +1
+  jmp -4
+  acc +6
+  """
+
+  test "part 1 sample solution" do
+    assert Day08.part_one(@sample) == 5
   end
 
-  test "part 2 solution" do
-    assert Day08.part_two() == :ok
-  end
+  # test "part 1 solution" do
+  #   assert Day08.part_one() == :ok
+  # end
+
+  # test "part 2 solution" do
+  #   assert Day08.part_two() == :ok
+  # end
 end
