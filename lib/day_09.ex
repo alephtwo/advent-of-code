73d1e5b6bf01855a527bc09208609f7ce0814325
Ben Christianson <thhuntertgm@gmail.com>
2020-12-08 23:16:01 -0600

    Start Day 9

--- lib/day_09.ex
+++ lib/day_09.ex
@@ -2,9 +2,23 @@ defmodule Day09 do
   @moduledoc """
   Day 9 of Advent of Code 2020.
   """
-  @input ""
+  @input File.read!("priv/09.txt")
 
-  def part_one(input \\ @input), do: input
+  def part_one(input \\ @input, bandwidth \\ 25) do
+    input
+    |> parse_input()
+    |> determine_vulnerability(bandwidth)
+  end
 
   def part_two(input \\ @input), do: input
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp determine_vulnerability(input, bandwidth) do
+    input
+  end
 end
--- test/day_09_test.exs
+++ test/day_09_test.exs
@@ -6,10 +6,30 @@ defmodule Day09Test do
   doctest Day09
 
   @sample """
+  35
+  20
+  15
+  25
+  47
+  40
+  62
+  55
+  65
+  95
+  102
+  117
+  150
+  182
+  127
+  219
+  299
+  277
+  309
+  576
   """
 
   test "part 1 sample solution" do
-    assert Day09.part_one(@sample) == :ok
+    assert Day09.part_one(@sample, 5) == 127
   end
 
   # test "part 1 solution" do
