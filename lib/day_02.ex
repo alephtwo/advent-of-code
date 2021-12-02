460b08820737db32c712cf7856fca8b7b6124a6c
Ben Christianson <thhuntertgm@gmail.com>
2021-12-01 23:10:03 -0600

    Solve day 2 part 1

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -1,7 +1,17 @@
 defmodule Day02 do
   def part_one(input) do
-    input
-    |> parse_input
+    {x, y} =
+      input
+      |> parse_input
+      |> Enum.reduce({0, 0}, fn instruction, {x, y} ->
+        case instruction do
+          %{direction: "forward", amplitude: a} -> {x + a, y}
+          %{direction: "down", amplitude: a} -> {x, y + a}
+          %{direction: "up", amplitude: a} -> {x, y - a}
+        end
+      end)
+
+    x * y
   end
 
   def part_two(input) do
@@ -11,5 +21,10 @@ defmodule Day02 do
 
   defp parse_input(raw) do
     raw
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [direction, amplitude] = String.split(line, " ")
+      %{direction: direction, amplitude: String.to_integer(amplitude)}
+    end)
   end
 end
--- test/day_02_test.exs
+++ test/day_02_test.exs
@@ -2,23 +2,28 @@ defmodule Day02Test do
   use ExUnit.Case
   doctest Day02
 
-  @sample_input nil
+  @sample_input """
+  forward 5
+  down 5
+  forward 8
+  up 3
+  down 8
+  forward 2
+  """
 
   @puzzle_input File.read!("priv/02.txt")
 
-  @tag :unimplemented
   test "part one example" do
-    assert Day02.part_one(@sample_input) == nil
+    assert Day02.part_one(@sample_input) == 150
   end
 
-  @tag :unimplemented
   test "part one solution" do
-    assert Day02.part_one(@puzzle_input) == nil
+    assert Day02.part_one(@puzzle_input) == 1_813_801
   end
 
   @tag :unimplemented
   test "part two example" do
-    assert Day02.part_two(@sample_input) == nil
+    assert Day02.part_two(@sample_input) == 900
   end
 
   @tag :unimplemented
