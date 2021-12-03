a62f97a0c1c8458b6658a441f18ebbc0c3081f8e
Ben Christianson <thhuntertgm@gmail.com>
2021-12-02 23:26:34 -0600

    Day 3 part 1

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -4,8 +4,12 @@ defmodule Day03 do
   """
 
   def part_one(input) do
-    input
-    |> parse_input
+    frequencies = input |> parse_input() |> columnar_frequencies()
+
+    gamma_rate = find_gamma_rate(frequencies)
+    epsilon_rate = find_epsilon_rate(frequencies)
+
+    gamma_rate * epsilon_rate
   end
 
   def part_two(input) do
@@ -15,5 +19,27 @@ defmodule Day03 do
 
   defp parse_input(raw) do
     raw
+    |> String.split("\n", trim: true)
+  end
+
+  defp columnar_frequencies(rows) do
+    rows
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.frequencies/1)
+  end
+
+  defp find_gamma_rate(frequencies), do: find_rate(frequencies, &Enum.max_by/2)
+  defp find_epsilon_rate(frequencies), do: find_rate(frequencies, &Enum.min_by/2)
+
+  defp find_rate(frequencies, func) do
+    frequencies
+    |> Enum.map(&Map.to_list/1)
+    |> Enum.map(fn col -> func.(col, fn {_, b} -> b end) end)
+    |> Enum.map(fn {a, _} -> a end)
+    |> Enum.join("")
+    |> String.to_integer(2)
   end
 end
--- test/day_03_test.exs
+++ test/day_03_test.exs
@@ -2,18 +2,29 @@ defmodule Day03Test do
   use ExUnit.Case
   doctest Day03
 
-  @sample_input ""
+  @sample_input """
+  00100
+  11110
+  10110
+  10111
+  10101
+  01111
+  00111
+  11100
+  10000
+  11001
+  00010
+  01010
+  """
 
   @puzzle_input File.read!("priv/03.txt")
 
-  @tag :unimplemented
   test "part one example" do
-    assert Day03.part_one(@sample_input) == nil
+    assert Day03.part_one(@sample_input) == 198
   end
 
-  @tag :unimplemented
   test "part one solution" do
-    assert Day03.part_one(@puzzle_input) == nil
+    assert Day03.part_one(@puzzle_input) == 2_724_524
   end
 
   @tag :unimplemented
