c723ca7d6493a66f784dba9b8340f969d1f5d7fa
Ben Christianson <thhuntertgm@gmail.com>
2021-12-04 14:54:12 -0600

    Stub day 5

---
+++ 2021/lib/day_05.ex
@@ -0,0 +1,18 @@
+defmodule Day05 do
+  @moduledoc """
+  Day 5 of the Advent of Code 2021.
+  """
+  def part_one(input) do
+    input
+    |> parse_input()
+  end
+
+  def part_two(input) do
+    input
+    |> parse_input()
+  end
+
+  defp parse_input(raw) do
+    raw
+  end
+end
--- 2021/test/day_04_test.exs
+++ 2021/test/day_04_test.exs
@@ -1,6 +1,7 @@
 defmodule Day04Test do
   use ExUnit.Case
   doctest Day04
+  @moduletag :finished
 
   @sample_input """
   7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
