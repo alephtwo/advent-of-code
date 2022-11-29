b48ade711d6ab4ab6d711ab1f3f2a2da115974b8
Ben Christianson <thhuntertgm@gmail.com>
2022-11-29 16:13:11 -0600

    Get very enterprisey, add logback

--- 2022/src/main/java/io/alephtwo/adventofcode/day/Day.java
+++ 2022/src/main/java/io/alephtwo/adventofcode/day/Day.java
@@ -6,6 +6,8 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class Day {
     private final String filename;
--- 2022/src/main/java/io/alephtwo/adventofcode/day/Day01.java
+++ 2022/src/main/java/io/alephtwo/adventofcode/day/Day01.java
@@ -1,14 +1,19 @@
 package io.alephtwo.adventofcode.day;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Day01 extends Day {
+    private static final Logger LOG = LoggerFactory.getLogger(Day01.class);
+
     public Day01() {
         super("01.txt");
     }
 
     public static void main(final String[] args) {
         final var day = new Day01();
-        System.out.println(day.partOne());
-        System.out.println(day.partTwo());
+        LOG.info("Part One: {}", day.partOne());
+        LOG.info("Part Two: {}", day.partTwo());
     }
 
     @Override
