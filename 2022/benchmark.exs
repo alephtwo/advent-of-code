aff6b2cc7d4bd8fcfc034c8ae575bca6f770e38e
Ben Christianson <thhuntertgm@gmail.com>
2022-11-30 23:09:54 -0600

    Solve Day 1

--- 2022/benchmark.exs
+++ 2022/benchmark.exs
@@ -1,4 +1,8 @@
+files = %{
+  1 => File.read!("priv/01.txt")
+}
+
 Benchee.run(%{
-  "day 1 part one" => fn -> Day01.part_one(File.read!("priv/01.txt")) end,
-  "day 1 part two" => fn -> Day01.part_two(File.read!("priv/01.txt")) end,
+  "day 1 part one" => fn -> Day01.part_one(Map.get(files, 1)) end,
+  "day 1 part two" => fn -> Day01.part_two(Map.get(files, 1)) end,
 })
--- 2022/test/day_01_test.exs
+++ 2022/test/day_01_test.exs
@@ -3,30 +3,24 @@ defmodule Day01Test do
   doctest Day01
 
   @puzzle_input File.read!("priv/01.txt")
-  @sample_input """
-  """
 
   # not yet implemented
-  @tag :skip
   test "part 1 example" do
-    assert Day01.part_one(@sample_input) == :ok
+    assert Day01.part_one(@sample_input) == 24_000
   end
 
   # not yet implemented
-  @tag :skip
   test "part 1 solution" do
-    assert Day01.part_one(@puzzle_input) == :ok
+    assert Day01.part_one(@puzzle_input) == 68_802
   end
 
   # not yet implemented
-  @tag :skip
   test "part 2 example" do
-    assert Day01.part_two(@sample_input) == :ok
+    assert Day01.part_two(@sample_input) == 45_000
   end
 
   # not yet implemented
-  @tag :skip
   test "part 2 solution" do
-    assert Day01.part_one(@puzzle_input) == :ok
+    assert Day01.part_two(@puzzle_input) == 205_370
   end
 end
