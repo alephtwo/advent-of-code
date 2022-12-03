497d11c776bb75c32447778b4487d41b08a39794
Ben Christianson <thhuntertgm@gmail.com>
2022-12-02 23:49:45 -0600

    Stub day 3

---
+++ 2022/test/day_04_test.exs
@@ -0,0 +1,28 @@
+defmodule Day04Test do
+  use ExUnit.Case
+  doctest Day04
+
+  @puzzle_input File.read!("priv/04.txt")
+
+  @sample_input """
+  """
+
+  test "part 1 example" do
+    assert Day04.part_one(@sample_input) == :ok
+  end
+
+  @tag :skip
+  test "part 1 solution" do
+    assert Day04.part_one(@puzzle_input) == :ok
+  end
+
+  @tag :skip
+  test "part 2 example" do
+    assert Day04.part_two(@sample_input) == :ok
+  end
+
+  @tag :skip
+  test "part 2 solution" do
+    assert Day04.part_two(@puzzle_input) == :ok
+  end
+end
