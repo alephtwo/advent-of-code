00fc570526be724455a23ba20808ede9945a0de2
Ben Christianson <thhuntertgm@gmail.com>
2022-12-05 17:46:10 -0600

    Add benchmarking for day 5

--- 2022/benchmark.exs
+++ 2022/benchmark.exs
@@ -2,7 +2,8 @@ files = %{
   1 => File.read!("priv/01.txt"),
   2 => File.read!("priv/02.txt"),
   3 => File.read!("priv/03.txt"),
-  4 => File.read!("priv/04.txt")
+  4 => File.read!("priv/04.txt"),
+  5 => File.read!("priv/05.txt")
 }
 
 Benchee.run(%{
@@ -12,6 +13,8 @@ Benchee.run(%{
   # "day 2 part two" => fn -> Day02.part_two(Map.get(files, 2)) end,
   # "day 3 part one" => fn -> Day03.part_one(Map.get(files, 3)) end,
   # "day 3 part two" => fn -> Day03.part_two(Map.get(files, 3)) end,
-  "day 4 part one" => fn -> Day04.part_one(Map.get(files, 4)) end,
-  "day 4 part two" => fn -> Day04.part_two(Map.get(files, 4)) end,
+  # "day 4 part one" => fn -> Day04.part_one(Map.get(files, 4)) end,
+  # "day 4 part two" => fn -> Day04.part_two(Map.get(files, 4)) end,
+  "day 5 part one" => fn -> Day05.part_one(Map.get(files, 5)) end,
+  "day 5 part two" => fn -> Day05.part_two(Map.get(files, 5)) end
 })
