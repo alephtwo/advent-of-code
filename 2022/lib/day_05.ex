ac22d2117193044a6c500693163b6692a20e4d44
Ben Christianson <thhuntertgm@gmail.com>
2022-12-04 23:20:00 -0600

    Solve Day 5

--- 2022/lib/day_05.ex
+++ 2022/lib/day_05.ex
@@ -3,26 +3,117 @@ defmodule Day05 do
   Day 5 of Advent of Code 2022.
   """
 
+  @block_width 4
+  @instruction_regex ~r/move (\d+) from (\d+) to (\d+)/
+
   @doc """
   """
   @spec part_one(String.t()) :: number()
   def part_one(input) do
-    input
-    |> parse_input()
-    |> IO.inspect()
+    %{stacks: stacks, instructions: instructions} = parse_input(input)
+
+    instructions
+    |> Enum.reduce(stacks, &process_instruction/2)
+    |> Enum.map(&Enum.at(&1, 0))
+    |> Enum.join()
   end
 
   @doc """
   """
   @spec part_two(String.t()) :: number()
   def part_two(input) do
-    input
-    |> parse_input()
-    |> IO.inspect()
+    %{stacks: stacks, instructions: instructions} = parse_input(input)
+
+    instructions
+    |> Enum.reduce(stacks, &process_instruction_enhanced/2)
+    |> Enum.map(&Enum.at(&1, 0))
+    |> Enum.join()
   end
 
   defp parse_input(input) do
-    input
-    |> String.split("\n", trim: true)
+    # Split into two halves. First is the def, next is the instructions
+    [preamble, instructions] = String.split(input, "\n\n", trim: true)
+
+    [cols | reversed_lines] = String.split(preamble, "\n") |> Enum.reverse()
+
+    last_col =
+      cols
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.max()
+
+    # determine the width in characters of the blocks
+    width = @block_width * last_col
+
+    rows =
+      reversed_lines
+      |> Enum.reverse()
+      |> Enum.map(&String.pad_trailing(&1, width))
+      |> Enum.map(fn string ->
+        string
+        |> String.codepoints()
+        |> Enum.chunk_every(4)
+        |> Enum.map(&Enum.join/1)
+        |> Enum.map(&parse_block/1)
+      end)
+
+    stacks =
+      Enum.map(1..last_col, fn i ->
+        rows
+        |> Enum.map(fn r -> Enum.at(r, i - 1) end)
+        |> Enum.filter(fn r -> r != nil end)
+      end)
+
+    %{
+      stacks: stacks,
+      instructions:
+        instructions
+        |> String.split("\n", trim: true)
+        |> Enum.map(&parse_instruction/1)
+    }
+  end
+
+  defp parse_instruction(line) do
+    [_, amount, src, dest] = Regex.run(@instruction_regex, line)
+
+    %{
+      amount: String.to_integer(amount),
+      src: String.to_integer(src),
+      dest: String.to_integer(dest)
+    }
+  end
+
+  defp parse_block(block) do
+    # whatever is at index 1 determines what this is
+    thing =
+      block
+      |> String.graphemes()
+      |> Enum.at(1)
+
+    if thing == " ", do: nil, else: thing
+  end
+
+  defp process_instruction(instruction, stacks) do
+    moving =
+      stacks
+      |> Enum.at(instruction.src - 1)
+      |> Enum.take(instruction.amount)
+      |> Enum.reverse()
+
+    stacks
+    |> List.update_at(instruction.src - 1, &Enum.drop(&1, instruction.amount))
+    |> List.update_at(instruction.dest - 1, fn x -> moving ++ x end)
+  end
+
+  defp process_instruction_enhanced(instruction, stacks) do
+    # the enhanced crane can pull multiples in order
+    moving =
+      stacks
+      |> Enum.at(instruction.src - 1)
+      |> Enum.take(instruction.amount)
+
+    stacks
+    |> List.update_at(instruction.src - 1, &Enum.drop(&1, instruction.amount))
+    |> List.update_at(instruction.dest - 1, fn x -> moving ++ x end)
   end
 end
--- 2022/test/day_05_test.exs
+++ 2022/test/day_05_test.exs
@@ -5,24 +5,30 @@ defmodule Day05Test do
   @puzzle_input File.read!("priv/05.txt")
 
   @sample_input """
+      [D]
+  [N] [C]
+  [Z] [M] [P]
+   1   2   3
+
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
   """
 
   test "part 1 example" do
-    assert Day05.part_one(@sample_input) == :ok
+    assert Day05.part_one(@sample_input) == "CMZ"
   end
 
-  @tag :skip
   test "part 1 solution" do
-    assert Day05.part_one(@puzzle_input) == :ok
+    assert Day05.part_one(@puzzle_input) == "VQZNJMWTR"
   end
 
-  @tag :skip
   test "part 2 example" do
-    assert Day05.part_two(@sample_input) == :ok
+    assert Day05.part_two(@sample_input) == "MCD"
   end
 
-  @tag :skip
   test "part 2 solution" do
-    assert Day05.part_two(@puzzle_input) == :ok
+    assert Day05.part_two(@puzzle_input) == "NLCDCLVMQ"
   end
 end
