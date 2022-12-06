84fa21564f05ec05915a5fd5000156b3e3eaae87
Ben Christianson <thhuntertgm@gmail.com>
2022-12-05 23:21:55 -0600

    Prepare Day 7

--- 2022/test/day_06_test.exs
+++ 2022/test/day_06_test.exs
@@ -10,50 +10,62 @@ defmodule Day06Test do
   @sample_input_4 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
   @sample_input_5 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
+  @tag :skip
   test "part 1 example 1" do
     assert Day06.part_one(@sample_input_1) == 7
   end
 
+  @tag :skip
   test "part 1 example 2" do
     assert Day06.part_one(@sample_input_2) == 5
   end
 
+  @tag :skip
   test "part 1 example 3" do
     assert Day06.part_one(@sample_input_3) == 6
   end
 
+  @tag :skip
   test "part 1 example 4" do
     assert Day06.part_one(@sample_input_4) == 10
   end
 
+  @tag :skip
   test "part 1 example 5" do
     assert Day06.part_one(@sample_input_5) == 11
   end
 
+  @tag :skip
   test "part 1 solution" do
     assert Day06.part_one(@puzzle_input) == 1623
   end
 
+  @tag :skip
   test "part 2 example 1" do
     assert Day06.part_two(@sample_input_1) == 19
   end
 
+  @tag :skip
   test "part 2 example 2" do
     assert Day06.part_two(@sample_input_2) == 23
   end
 
+  @tag :skip
   test "part 2 example 3" do
     assert Day06.part_two(@sample_input_3) == 23
   end
 
+  @tag :skip
   test "part 2 example 4" do
     assert Day06.part_two(@sample_input_4) == 29
   end
 
+  @tag :skip
   test "part 2 example 5" do
     assert Day06.part_two(@sample_input_5) == 26
   end
 
+  @tag :skip
   test "part 2 solution" do
     assert Day06.part_two(@puzzle_input) == 3774
   end
