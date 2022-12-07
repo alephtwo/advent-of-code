32e09220b6abafc5d88485403c42f6c97f95ff04
Ben Christianson <thhuntertgm@gmail.com>
2022-12-06 23:20:58 -0600

    Start building

--- 2022/lib/day_07.ex
+++ 2022/lib/day_07.ex
@@ -9,6 +9,7 @@ defmodule Day07 do
   def part_one(input) do
     input
     |> parse_input()
+    |> Enum.reduce(%{pwd: "/", sizes: %{}}, &process_line/2)
     |> IO.inspect()
   end
 
@@ -21,8 +22,44 @@ defmodule Day07 do
     |> IO.inspect()
   end
 
-  defp parse_input(input) do
-    input
-    |> String.split("\n", trim: true)
+  defp parse_input(input), do: String.split(input, "\n", trim: true)
+
+  # Handle commands.
+  defp process_line("$ " <> cmd, ctx) do
+    case cmd do
+      "ls" ->
+        # Do nothing, this is just context for the next few lines.
+        ctx
+
+      "cd " <> dir ->
+        case dir do
+          # Move to root
+          "/" ->
+            %{ctx | pwd: "/"}
+
+          # Move up a path
+          ".." ->
+            %{ctx | pwd: ctx.pwd |> Path.split() |> Enum.drop(-1) |> Path.join()}
+
+          # Move down to the directory selected
+          _ ->
+            %{ctx | pwd: Path.join(ctx.pwd, dir)}
+        end
+
+      _ ->
+        throw("what cmd is this? #{cmd}")
+    end
+  end
+
+  defp process_line(line, ctx) do
+    case line do
+      "dir " <> _dir ->
+        ctx
+
+      _ ->
+        [size, _] = String.split(line, " ", trim: true)
+        bytes = String.to_integer(size)
+        %{ ctx | sizes: Map.update(ctx.sizes, ctx.pwd, bytes, fn x -> bytes + x end)}
+    end
   end
 end
--- 2022/test/day_07_test.exs
+++ 2022/test/day_07_test.exs
@@ -5,10 +5,33 @@ defmodule Day07Test do
   @puzzle_input File.read!("priv/07.txt")
 
   @sample_input """
+  $ cd /
+  $ ls
+  dir a
+  14848514 b.txt
+  8504156 c.dat
+  dir d
+  $ cd a
+  $ ls
+  dir e
+  29116 f
+  2557 g
+  62596 h.lst
+  $ cd e
+  $ ls
+  584 i
+  $ cd ..
+  $ cd ..
+  $ cd d
+  $ ls
+  4060174 j
+  8033020 d.log
+  5626152 d.ext
+  7214296 k
   """
 
   test "part 1 example" do
-    assert Day07.part_one(@sample_input) == :ok
+    assert Day07.part_one(@sample_input) == 95437
   end
 
   @tag :skip
@@ -18,7 +41,7 @@ defmodule Day07Test do
 
   @tag :skip
   test "part 2 example" do
-    assert Day07.part_two(@sample_input_1) == :ok
+    assert Day07.part_two(@sample_input) == :ok
   end
 
   @tag :skip
