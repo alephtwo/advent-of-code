0ca170e6e81ba52f6fc0c96dc320f8eb051e3693
Ben Christianson <thhuntertgm@gmail.com>
2022-12-10 01:32:27 -0600

    Implement

--- 2022/lib/day_09.ex
+++ 2022/lib/day_09.ex
@@ -14,6 +14,9 @@ defmodule Day09 do
     input
     |> parse_input()
     |> Enum.reduce(%State{}, &drag_rope/2)
+    |> then(fn %State{tail_locations: t} -> t end)
+    |> Enum.uniq()
+    |> Enum.count()
   end
 
   @doc """
@@ -31,14 +34,7 @@ defmodule Day09 do
     |> Enum.reduce(state, &drag_rope/2)
   end
 
-  defp drag_rope(
-         direction,
-         state = %State{
-           tail_locations: [],
-           head: {head_x, head_y},
-           tail: {tail_x, tail_y}
-         }
-       ) do
+  defp drag_rope(direction, state = %State{head: {head_x, head_y}}) do
     head =
       case direction do
         "R" -> {head_x + 1, head_y}
@@ -47,15 +43,61 @@ defmodule Day09 do
         "D" -> {head_x, head_y - 1}
       end
 
-    %State{state | head: head}
+    tail = catch_up(state.tail, head)
+
+    %State{
+      head: head,
+      tail: tail,
+      tail_locations: [tail | state.tail_locations]
+    }
+  end
+
+  defp catch_up(tail = {tail_x, tail_y}, head) do
+    case grid_distance(head, tail) do
+      {0, 0} ->
+        tail
+
+      {dx, dy} when dx == 1 and dy == 1 ->
+        tail
+
+      {dx, dy} when dx == 0 and abs(dy) == 1 ->
+        tail
+
+      {dx, dy} when abs(dx) == 1 and dy == 0 ->
+        tail
+
+      {0, 2} ->
+        {tail_x, tail_y + 1}
+
+      {0, -2} ->
+        {tail_x, tail_y - 1}
+
+      {2, 0} ->
+        {tail_x + 1, tail_y}
+
+      {-2, 0} ->
+        {tail_x - 1, tail_y}
+
+      {dx, dy} when dx > 0 and dy > 0 ->
+        {tail_x + 1, tail_y + 1}
+
+      {dx, dy} when dx > 0 and dy < 0 ->
+        {tail_x + 1, tail_y - 1}
+
+      {dx, dy} when dx < 0 and dy > 0 ->
+        {tail_x - 1, tail_y + 1}
+
+      {dx, dy} when dx < 0 and dy < 0 ->
+        {tail_x - 1, tail_y - 1}
+    end
   end
 
   def grid_distance({head_x, head_y}, {tail_x, tail_y}),
-    do: {abs(head_x - tail_x), abs(head_y - tail_y)}
+    do: {head_x - tail_x, head_y - tail_y}
 
   def is_adjacent(head, tail) do
     {x, y} = grid_distance(head, tail)
-    x <= 1 and y <= 1
+    abs(x) <= 1 and abs(y) <= 1
   end
 
   defp parse_input(input) do
