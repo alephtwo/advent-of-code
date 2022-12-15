94a164b9ce2e942039e54b7831e25224fd2388b2
Ben Christianson <thhuntertgm@gmail.com>
2022-12-15 12:40:39 -0600

    Build input

--- 2022/lib/day_15.ex
+++ 2022/lib/day_15.ex
@@ -3,6 +3,14 @@ defmodule Day15 do
   Day 15 of Advent of Code 2022.
   """
 
+  defmodule Sensor do
+    defstruct [:closest_beacon]
+  end
+
+  defmodule Beacon do
+    defstruct []
+  end
+
   @doc """
   """
   @spec part_one(String.t(), integer()) :: number()
@@ -27,7 +35,20 @@ defmodule Day15 do
     |> String.split("\n", trim: true)
     |> Enum.map(fn line ->
       [_, sx, sy, cbx, cby] = Regex.run(@line_regex, line)
-      {{sx, sy}, {cbx, cby}}
+
+      {
+        {String.to_integer(sx), String.to_integer(sy)},
+        {String.to_integer(cbx), String.to_integer(cby)}
+      }
+    end)
+    |> place_objects()
+  end
+
+  defp place_objects(lines) do
+    Enum.reduce(lines, %{}, fn {{sx, sy}, {cbx, cby}}, coords ->
+      coords
+      |> Map.put({sx, sy}, %Sensor{closest_beacon: {cbx, cby}})
+      |> Map.put({cbx, cby}, %Beacon{})
     end)
   end
 end
