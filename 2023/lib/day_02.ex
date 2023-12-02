c51564fc501bcdb33719f1e3070e740b692badf8
Ben Christianson <thhuntertgm@gmail.com>
2023-12-01 23:19:52 -0600

    Complete day 2

--- 2023/lib/day_02.ex
+++ 2023/lib/day_02.ex
@@ -7,6 +7,15 @@ defmodule AdventOfCode2023.Day02 do
   def part_one(input) do
     input
     |> parse_input()
+    |> Enum.filter(
+      &check_game(&1, %{
+        "red" => 12,
+        "green" => 13,
+        "blue" => 14
+      })
+    )
+    |> Enum.map(fn game -> game.id end)
+    |> Enum.sum()
   end
 
   @doc """
@@ -14,10 +23,55 @@ defmodule AdventOfCode2023.Day02 do
   def part_two(input) do
     input
     |> parse_input()
+    |> Enum.map(&get_min_cubes/1)
+    |> Enum.map(fn limits -> limits |> Map.values() |> Enum.product() end)
+    |> Enum.sum()
   end
 
   defp parse_input(input) do
     input
     |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [_, id, contents] = Regex.run(~r/Game (\d+)\: (.*)/, line)
+
+      pulls =
+        contents
+        |> String.split(";", trim: true)
+        |> Enum.map(&parse_pull/1)
+
+      %{
+        id: id |> Integer.parse() |> elem(0),
+        pulls: pulls
+      }
+    end)
+  end
+
+  defp parse_pull(pull) do
+    pull
+    |> String.split(",", trim: true)
+    |> Enum.map(fn handful ->
+      [_, count, color] = Regex.run(~r/(\d+) (.*)/, handful)
+      {color, count |> Integer.parse() |> elem(0)}
+    end)
+    |> Map.new()
+  end
+
+  defp check_game(game, known_contents) do
+    # Check all pulls in the game against known contents
+    # If the game is good, "not any" aka "none of these but return fast"
+    !Enum.any?(game.pulls, fn pull ->
+      # in the pull, none of the colors will have exceeded the known contents
+      Enum.any?(pull, fn {color, amount} -> amount > Map.get(known_contents, color) end)
+    end)
+  end
+
+  defp get_min_cubes(game) do
+    Enum.reduce(game.pulls, %{"red" => 0, "green" => 0, "blue" => 0}, fn pull, acc ->
+      %{
+        "red" => max(Map.get(acc, "red"), Map.get(pull, "red", -1)),
+        "green" => max(Map.get(acc, "green"), Map.get(pull, "green", -1)),
+        "blue" => max(Map.get(acc, "blue"), Map.get(pull, "blue", -1))
+      }
+    end)
   end
 end
--- 2023/test/day_02_test.exs
+++ 2023/test/day_02_test.exs
@@ -6,24 +6,26 @@ defmodule AdventOfCode2023.Day02Test do
   @puzzle_input File.read!("priv/02.txt")
 
   @sample_input """
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
   """
 
   test "part 1 example" do
-    assert Day02.part_one(@sample_input_1) == nil
+    assert Day02.part_one(@sample_input) == 8
   end
 
-  @tag :skip
   test "part 1 solution" do
-    assert Day02.part_one(@puzzle_input) == nil
+    assert Day02.part_one(@puzzle_input) == 2_006
   end
 
-  @tag :skip
   test "part 2 example" do
-    assert Day02.part_two(@sample_input_2) == nil
+    assert Day02.part_two(@sample_input) == 2_286
   end
 
-  @tag :skip
   test "part 2 solution" do
-    assert Day02.part_two(@puzzle_input) == nil
+    assert Day02.part_two(@puzzle_input) == 84_911
   end
 end
