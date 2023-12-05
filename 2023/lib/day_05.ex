eea88c09814aa5dc8ae133a95be6d86d8c59b358
Ben Christianson <thhuntertgm@gmail.com>
2023-12-04 23:12:51 -0600

    Start Day 5

--- 2023/lib/day_05.ex
+++ 2023/lib/day_05.ex
@@ -2,6 +2,17 @@ defmodule AdventOfCode2023.Day05 do
   @moduledoc """
   """
 
+  defmodule Almanac do
+    defstruct seeds: [],
+              seed_to_soil: nil,
+              soil_to_fertilizer: nil,
+              fertilizer_to_water: nil,
+              water_to_light: nil,
+              light_to_temperature: nil,
+              temperature_to_humidity: nil,
+              humidity_to_location: nil
+  end
+
   @doc """
   """
   def part_one(input) do
@@ -17,7 +28,50 @@ defmodule AdventOfCode2023.Day05 do
   end
 
   defp parse_input(input) do
+    [
+      seeds,
+      seed_to_soil,
+      soil_to_fertilizer,
+      fertilizer_to_water,
+      water_to_light,
+      light_to_temperature,
+      temperature_to_humidity,
+      humidity_to_location
+    ] = String.split(input, "\n\n", trim: true)
+
+    %Almanac{
+      seeds:
+        seeds
+        |> String.replace_prefix("seeds: ", "")
+        |> parse_space_separated_numbers(),
+      seed_to_soil: parse_almanac_ranges(seed_to_soil),
+      soil_to_fertilizer: parse_almanac_ranges(soil_to_fertilizer),
+      fertilizer_to_water: parse_almanac_ranges(fertilizer_to_water),
+      water_to_light: parse_almanac_ranges(water_to_light),
+      light_to_temperature: parse_almanac_ranges(light_to_temperature),
+      temperature_to_humidity: parse_almanac_ranges(temperature_to_humidity),
+      humidity_to_location: parse_almanac_ranges(humidity_to_location)
+    }
+  end
+
+  defp parse_almanac_ranges(mapstring) do
+    # throw away the first line, it's just a header
+    [_ | ranges] = String.split(mapstring, "\n", trim: true)
+
+    ranges
+    |> Enum.map(&parse_space_separated_numbers/1)
+    # take for granted that there are exactly three numbers in each row
+    |> Enum.map(fn [dest_start, source_start, range_length] ->
+      0..(range_length - 1)
+      |> Enum.map(fn i -> {source_start + i, dest_start + i} end)
+    end)
+    |> Enum.reduce(%{}, fn a, acc -> Map.merge(acc, Map.new(a)) end)
+  end
+
+  defp parse_space_separated_numbers(input) do
     input
-    |> String.split("\n", trim: true)
+    |> String.split(" ", trim: true)
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.map(&elem(&1, 0))
   end
 end
--- 2023/test/day_05_test.exs
+++ 2023/test/day_05_test.exs
@@ -6,10 +6,43 @@ defmodule AdventOfCode2023.Day05Test do
   @puzzle_input File.read!("priv/05.txt")
 
   @sample_input """
+  seeds: 79 14 55 13
+
+  seed-to-soil map:
+  50 98 2
+  52 50 48
+
+  soil-to-fertilizer map:
+  0 15 37
+  37 52 2
+  39 0 15
+
+  fertilizer-to-water map:
+  49 53 8
+  0 11 42
+  42 0 7
+  57 7 4
+
+  water-to-light map:
+  88 18 7
+  18 25 70
+
+  light-to-temperature map:
+  45 77 23
+  81 45 19
+  68 64 13
+
+  temperature-to-humidity map:
+  0 69 1
+  1 0 69
+
+  humidity-to-location map:
+  60 56 37
+  56 93 4
   """
 
   test "part 1 example" do
-    assert Day05.part_one(@sample_input) == nil
+    assert Day05.part_one(@sample_input) == 35
   end
 
   @tag :skip
